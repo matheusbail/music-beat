541810ef6a1cac4218ce50f7fbdca55b04728755
matheus bail <matheus312henrique@gmail.com>
2023-03-13 19:33:10 -0300

    pages navegation

---
+++ app/pages/login/login.js
@@ -0,0 +1,5 @@
+function moveP() {
+    window.location.href = "/music-beat/app/pages/player/player.html";
+}
+
+
---
+++ assets/resources/scripts/index.js
@@ -0,0 +1,8 @@
+function move() {
+    window.location.href = "/music-beat/app/pages/login/login.html";
+}
+
+
+		function Previous() {
+			window.history.back()
+        }
