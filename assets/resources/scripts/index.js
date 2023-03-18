9bfea8af3318f9e4ae11c04235790dbb069c013b
matheus bail <matheus312henrique@gmail.com>
2023-03-17 21:29:36 -0300

    modal initial page

--- assets/resources/scripts/index.js
+++ assets/resources/scripts/index.js
@@ -1,8 +1,20 @@
 function move() {
     window.location.href = "/music-beat/app/pages/login/login.html";
 }
-
-
 		function Previous() {
 			window.history.back()
         }
+
+//window.setTimeout(move(),1000);
+
+//window.setInterval(move(),1000);
+
+document.addEventListener('DOMContentLoaded', function () {
+    let Modalelem = document.querySelector('.modal');
+    let instance = M.Modal.init(Modalelem);
+    instance.open();
+ 
+});
+
+
+       
