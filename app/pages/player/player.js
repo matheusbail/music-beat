94bf05bc62b929ddcc17f52a93d1a2e843701467
matheus bail <matheus312henrique@gmail.com>
2023-06-23 14:57:23 -0300

    regex ok class function ok

--- app/pages/player/player.js
+++ app/pages/player/player.js
@@ -9,6 +9,15 @@ window.addEventListener("keydown", function(e) //funcao anonima com arg
       }
 })
 
+document.getElementById("playAndPauseBtn").addEventListener("click", function(){ // criaçao de obj via funcao
+  let musica = new Object();
+    musica.Titulo = "Wake Up"
+    musica.Lancamento = 1992;
+    musica.Album = "Rage Against The Machine";
+  let musToStr = JSON.stringify(musica, null,2);
+  document.getElementById("OO").innerHTML = musToStr;
+})
+
 
 // funcao anonima, e confirm
 (function() {
